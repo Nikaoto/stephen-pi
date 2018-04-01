47d89529f7df7dfc17d4142c1787458a4373fe40
Nikaoto <nikaoto@gmail.com>
2018-04-01 16:12:03 +0400

    socketio integration

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, send_from_directory
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__, static_folder="public", static_url_path="")
 app.config["SECRET_KEY"] = "secret!"
@@ -8,6 +8,10 @@ socketio = SocketIO(app)
 @app.route("/")
 def index():
 	return send_from_directory("public", "index.html")
+
+@socketio.on("question")
+def handle_question(question):
+	emit("answer", question)
 
 if (__name__ == "__main__"):
 	socketio.run(app)
