eff258669a125a7dc97bd309b140c104390c4ed0
Nikaoto <nikaoto@gmail.com>
2018-04-01 13:58:31 +0400

    add socketio to server; fix default question bug

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, send_from_directory
+from flask_socketio import SocketIO
 
 app = Flask(__name__, static_folder="public", static_url_path="")
+app.config["SECRET_KEY"] = "secret!"
+socketio = SocketIO(app)
 
 @app.route("/")
 def index():
 	return send_from_directory("public", "index.html")
 
 if (__name__ == "__main__"):
-	app.run()
+	socketio.run(app)
