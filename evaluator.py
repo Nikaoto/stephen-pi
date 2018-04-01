703a7734fd937ef4276d31a801f964e25abb5064
Nikaoto <nikaoto@gmail.com>
2018-04-01 17:29:32 +0400

    a

--- evaluator.py
+++ evaluator.py
@@ -1,2 +1,17 @@
 import answers
-import hawking
+
+def includes(question, keywords):
+	for word in keywords:
+		if (word not in question):
+			return False
+	return True
+
+def evaluate(question):
+	for i in range (0, len(answers["keygroups"])):
+		if (includes(question, keygroups[i]["keywords"])):
+			answer = keygroups[i]["answer"]
+			while (answer == ""):
+				i -= 1
+				answer = keygroups[i]["answer"]
+			return answer
+	return answers.default_answer
